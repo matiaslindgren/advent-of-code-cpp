ea9a7b8ff6af1e42b57c50c6bb5cd172e335ce7d
Matias Lindgren <matias.lindgren@iki.fi>
2023-09-25 21:52:17 +0200

    complexify

--- src/2015/01.cpp
+++ src/2015/01.cpp
@@ -1,27 +1,34 @@
 import std;
 
+// TODO(llvm17)
+namespace my_ranges {
+template <class Container>
+  requires(!std::ranges::view<Container>)
+constexpr auto to() {
+  return std::__range_adaptor_closure_t([]<std::ranges::input_range R>(R &&r) {
+    Container c;
+    std::ranges::copy(r, std::back_inserter(c));
+    return c;
+  });
+}
+} // namespace my_ranges
+
 int main() {
-  using char_reader = std::istream_iterator<std::string::value_type>;
-  std::string parens{char_reader{std::cin}, char_reader{}};
-  int part1{0};
-  int part2{0};
-  int pos{0};
-  for (auto c : parens) {
-    ++pos;
-    switch (c) {
-    case '(': {
-      ++part1;
-    } break;
-    case ')': {
-      --part1;
-    } break;
-    default:
-      throw std::runtime_error{"invalid input"};
-    }
-    if (part2 == 0 && part1 < 0) {
-      part2 = pos;
-    }
-  }
+  const auto parse_current_floor = [floor = 0](auto ch) mutable {
+    return floor += (ch == '(') - (ch == ')');
+  };
+  const auto floors = std::views::istream<char>(std::cin) |
+                      std::views::transform(parse_current_floor) |
+                      // TODO(llvm17)
+                      // std::ranges::to<std::vector<int>>();
+                      my_ranges::to<std::vector<int>>();
+
+  const auto part1 = floors.back();
+  const auto part2 =
+      1 +
+      std::distance(floors.begin(),
+                    std::ranges::find_if(floors, [](auto x) { return x < 0; }));
+
   std::cout << part1 << ' ' << part2 << '\n';
   return 0;
 }
