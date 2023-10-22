593e5f5a2ba9c88d784fdc2ad16f128d597bd84c
Matias Lindgren <matias.lindgren@iki.fi>
2023-10-22 14:28:41 +0200

    solve 2015/20

---
+++ src/2015/20.cpp
@@ -0,0 +1,31 @@
+import std;
+
+namespace ranges = std::ranges;
+
+int deliver_presents(
+    const int target,
+    const int delivery_count,
+    const int house_count = std::numeric_limits<int>::max()
+) {
+  std::vector<int> presents(target);
+  for (int elf{1}; delivery_count * elf < target; ++elf) {
+    for (int house{1}; house <= house_count && delivery_count * house * elf < target; ++house) {
+      presents[house * elf] += delivery_count * elf;
+    }
+  }
+  const auto house_match{ranges::find_if(presents, [&target](auto p) { return p >= target; })};
+  return ranges::distance(presents.begin(), house_match);
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+
+  int target;
+  std::cin >> target;
+
+  const auto part1{deliver_presents(target, 10)};
+  const auto part2{deliver_presents(target, 11, 50)};
+  std::print("{} {}\n", part1, part2);
+
+  return 0;
+}
