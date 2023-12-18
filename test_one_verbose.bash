ee440d4c44f3f610cae3770433fa4c11fc605f57
Matias Lindgren <matias.lindgren@iki.fi>
2023-12-18 18:49:16 +0100

    show make output in verbose test

--- test_one_verbose.bash
+++ test_one_verbose.bash
@@ -6,7 +6,7 @@ if [ $# -ne 1 ]; then
   exit 2
 fi
 
-target=$1
+target="$1"
 
 test_dir="$(mktemp --directory)"
 function rm_test_dir {
@@ -15,24 +15,32 @@ function rm_test_dir {
 trap rm_test_dir EXIT
 
 time_log="${test_dir}/time.log"
+make_log="${test_dir}/make.log"
+
 time_verbose_flag=-v
 if [ "$(uname)" = 'Darwin' ]; then
   time_verbose_flag=-l
 fi
 
-printf '%s\n' $target
-result=$(\
-  /usr/bin/env time \
+/usr/bin/env time \
   $time_verbose_flag \
   -o "$time_log" \
-  make --silent run_${target}\
-)
+  make "run_${target}" > "$make_log" 2>&1
+
+result=$(tail -n 1 "$make_log")
 expect=$(cat txt/correct/${target})
+
+printf '%s\n' "$target"
 printf 'result: %s\n' "$result"
 printf 'expect: %s\n' "$expect"
+
+printf 'make:\n'
+head -n 1 "$make_log"
+
 printf 'time:\n'
 cat "$time_log"
 printf '\n'
+
 if [ "$result" != "$expect" ]; then
   exit 1
 fi
