eae30113298cbdebe76a5be43d2bbb6f227d55f3
Matias Lindgren <matias.lindgren@iki.fi>
2024-01-05 20:55:42 +0100

    remove std::variant trickery

--- src/2016/12.cpp
+++ src/2016/12.cpp
@@ -1,38 +1,32 @@
 import std;
 import aoc;
 
-namespace ranges = std::ranges;
-namespace views = std::views;
+struct Operand {
+  enum {
+    address,
+    literal,
+  } type;
+  std::size_t index{};
+  int value{};
+};
 
-struct Register {
-  std::size_t index;
+struct Instruction {
+  enum {
+    copy,
+    jump_if_not_zero,
+    add,
+  } type;
+  Operand lhs;
+  Operand rhs;
 };
-struct Literal {
-  int value;
-};
-using Operand = std::variant<Register, Literal>;
-
-struct Add {
-  Operand dst;
-  Operand src;
-};
-struct Copy {
-  Operand dst;
-  Operand src;
-};
-struct JumpIfNotZero {
-  Operand test;
-  Operand count;
-};
-using Instruction = std::variant<Add, Copy, JumpIfNotZero>;
 
 std::istream& operator>>(std::istream& is, Operand& op) {
   if (std::string s; is >> s && !s.empty()) {
     if (const char reg_ch{s.front()}; 'a' <= reg_ch && reg_ch <= 'd') {
-      op = Register{static_cast<unsigned>(reg_ch - 'a')};
-      return is;
+      op = {.type = Operand::address, .index = static_cast<unsigned>(reg_ch - 'a')};
+    } else {
+      op = {.type = Operand::literal, .value = std::stoi(s)};
     }
-    op = Literal{std::stoi(s)};
     return is;
   }
   if (is.eof()) {
@@ -47,19 +41,23 @@ std::istream& operator>>(std::istream& is, Instruction& ins) {
     if (std::string type; ls >> type) {
       if (type == "cpy") {
         if (Operand src, dst; ls >> src >> dst) {
-          ins = Copy{dst, src};
+          ins = {Instruction::copy, dst, src};
           return is;
         }
       }
       if (type == "jnz") {
         if (Operand test, count; ls >> test >> count) {
-          ins = JumpIfNotZero{test, count};
+          ins = {Instruction::jump_if_not_zero, test, count};
           return is;
         }
       }
       if (type == "inc" || type == "dec") {
         if (Operand dst; ls >> dst) {
-          ins = Add{dst, Literal{type == "dec" ? -1 : 1}};
+          ins = {
+              Instruction::add,
+              dst,
+              {.type = Operand::literal, .value = type == "dec" ? -1 : 1},
+          };
           return is;
         }
       }
@@ -71,109 +69,56 @@ std::istream& operator>>(std::istream& is, Instruction& ins) {
   throw std::runtime_error("failed parsing Instruction");
 }
 
-using Memory = std::array<int, 4>;
-
-// https://www.reddit.com/r/cpp/comments/kst2pu/comment/giilcxv
-// accessed 2023-11-30
-template <std::size_t n>
-constexpr decltype(auto) visit(auto&& func, auto&& var) {
-#define aoc2016_variant_visit_case(c)       \
-  case (c): {                               \
-    if constexpr (n > (c)) {                \
-      return func(*std::get_if<(c)>(&var)); \
-    } else {                                \
-      std::unreachable();                   \
-    }                                       \
-  } break
-  switch (var.index()) {
-    aoc2016_variant_visit_case(0);
-    aoc2016_variant_visit_case(1);
-    aoc2016_variant_visit_case(2);
+auto read(const auto& memory, const Operand& op) {
+  if (op.type == Operand::address) {
+    return memory.at(op.index);
+  } else {
+    return op.value;
   }
-  std::unreachable();
-#undef aoc2016_variant_visit_case
 }
 
-struct Read {
-  Read() = delete;
-  explicit Read(const Memory& m) : memory{m} {
+void write(auto& memory, const Operand& op, auto value) {
+  if (op.type == Operand::address) {
+    memory[op.index] = value;
+  } else {
+    throw std::runtime_error("cannot write to a literal");
   }
+}
 
-  int operator()(const Register& reg) const {
-    return memory[reg.index];
+auto execute(auto& memory, const Instruction& ins) {
+  switch (ins.type) {
+    case Instruction::copy: {
+      write(memory, ins.lhs, read(memory, ins.rhs));
+    } break;
+    case Instruction::jump_if_not_zero: {
+      if (read(memory, ins.lhs) != 0) {
+        return read(memory, ins.rhs);
+      }
+    } break;
+    case Instruction::add: {
+      write(memory, ins.lhs, read(memory, ins.lhs) + read(memory, ins.rhs));
+    } break;
   }
-  int operator()(const Literal& lit) const {
-    return lit.value;
-  }
+  return 1;
+}
 
- private:
-  const Memory& memory;
-};
-
-struct Write {
-  Write() = delete;
-  explicit Write(Memory& mem, int val) : memory{mem}, value{val} {
-  }
-
-  void operator()(const Register& reg) const {
-    memory[reg.index] = value;
-  }
-  void operator()(const Literal&) const {
-    throw std::invalid_argument{"can't write to a literal"};
-  }
-
- private:
-  Memory& memory;
-  int value;
-};
-
-struct Execute {
-  Execute() = delete;
-  explicit Execute(Memory& mem) : memory{mem} {
-  }
-
-  std::ptrdiff_t operator()(const Add& add) const {
-    const int lhs{visit<2>(Read{memory}, add.dst)};
-    const int rhs{visit<2>(Read{memory}, add.src)};
-    visit<2>(Write{memory, lhs + rhs}, add.dst);
-    return 1;
-  }
-  std::ptrdiff_t operator()(const Copy& copy) const {
-    const int value{visit<2>(Read{memory}, copy.src)};
-    visit<2>(Write{memory, value}, copy.dst);
-    return 1;
-  }
-  std::ptrdiff_t operator()(const JumpIfNotZero& jump) const {
-    if (const int test{visit<2>(Read{memory}, jump.test)}; test != 0) {
-      return visit<2>(Read{memory}, jump.count);
-    }
-    return 1;
-  }
-
- private:
-  Memory& memory;
-};
-
-void run(const auto& instructions, auto& memory) {
+int run(const auto& instructions, std::array<int, 4> memory) {
   for (std::ptrdiff_t pos{}; 0 <= pos && pos < instructions.size();) {
-    pos += visit<3>(Execute{memory}, instructions[pos]);
+    const auto jump{execute(memory, instructions[pos])};
+    pos += jump;
   }
+  return memory[0];
 }
 
 int main() {
   aoc::init_io();
 
-  const auto instructions{views::istream<Instruction>(std::cin) | ranges::to<std::vector>()};
+  const auto instructions{
+      std::views::istream<Instruction>(std::cin) | std::ranges::to<std::vector>()
+  };
 
-  std::array<int, 4> memory;
-
-  run(instructions, memory);
-  const auto part1{memory[0]};
-
-  memory.fill(0);
-  memory[2] = 1;
-  run(instructions, memory);
-  const auto part2{memory[0]};
+  const auto part1{run(instructions, {0, 0, 0, 0})};
+  const auto part2{run(instructions, {0, 0, 1, 0})};
 
   std::print("{} {}\n", part1, part2);
 
