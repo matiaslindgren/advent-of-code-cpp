3f77e2926044d85c02952ae65a19840f08c1264e
Matias Lindgren <matias.lindgren@iki.fi>
2024-01-06 02:04:52 +0100

    optimize 2023/25

--- src/2023/25.cpp
+++ src/2023/25.cpp
@@ -8,116 +8,154 @@ using std::operator""s;
 namespace ranges = std::ranges;
 namespace views = std::views;
 
-constexpr auto sum{std::bind(my_std::ranges::fold_left, std::placeholders::_1, 0, std::plus{})};
-
 struct Graph {
-  std::unordered_map<std::string, std::unordered_map<std::string, int>> edges;
+  using Node = std::size_t;
+  using Weight = int;
+  using EdgeMap = std::unordered_map<std::string, std::unordered_map<std::string, Weight>>;
 
-  auto nodes() const {
-    return edges | views::keys | ranges::to<std::vector<std::string>>();
-  }
+  std::size_t size{};
+  std::vector<std::vector<Weight>> edges{};
+  std::vector<std::unordered_set<Node>> adjacent{};
 
-  void add_edge(const auto& n1, const auto& n2, const auto& w) {
-    edges[n1][n2] = edges[n2][n1] = w;
-  }
+  explicit Graph() = default;
 
-  int node_weight(const auto& n) const {
-    return sum(edges.at(n) | views::values);
-  }
-
-  int cut_weight(const auto& n1, const auto& cut) const {
-    int cut_w{};
-    if (!cut.contains(n1) && edges.contains(n1)) {
-      for (const auto& [n2, w] : edges.at(n1)) {
-        if (cut.contains(n2)) {
-          cut_w += w;
+  explicit Graph(const EdgeMap& edge_map) {
+    std::unordered_map<std::string, Node> node_ids;
+    for (const auto& adjacent : edge_map | views::values) {
+      for (const auto& node : adjacent | views::keys) {
+        if (!node_ids.contains(node)) {
+          node_ids[node] = node_ids.size();
         }
       }
     }
-    return cut_w;
+    size = node_ids.size();
+    edges.resize(size, std::vector<Weight>(size, 0));
+    adjacent.resize(size);
+    for (const auto& [node1, adjacent] : edge_map) {
+      for (const auto& [node2, weight] : adjacent) {
+        const auto n1{node_ids.at(node1)};
+        const auto n2{node_ids.at(node2)};
+        insert_edge(n1, n2, weight);
+        insert_edge(n2, n1, weight);
+      }
+    }
+  }
+
+  void insert_edge(const auto& n1, const auto& n2, const auto& weight) {
+    edges[n1][n2] = weight;
+    adjacent[n1].insert(n2);
+  }
+
+  void erase_edge(const auto& n1, const auto& n2) {
+    edges[n1][n2] = 0;
+    adjacent[n1].erase(n2);
+  }
+
+  auto nodes() const {
+    return views::iota(Node{}, size) | ranges::to<std::vector>();
   }
 };
 
 std::istream& operator>>(std::istream& is, Graph& graph) {
-  Graph g;
+  Graph::EdgeMap edges;
   for (std::string line; std::getline(is, line) && !line.empty();) {
     std::stringstream ls{line};
     if (std::string lhs; ls >> lhs && lhs.ends_with(":"s)) {
       lhs.pop_back();
       for (auto rhs : views::istream<std::string>(ls)) {
-        g.add_edge(lhs, rhs, 1);
+        edges[lhs][rhs] = edges[rhs][lhs] = Graph::Weight{1};
       }
     } else {
       is.setstate(std::ios_base::failbit);
       break;
     }
   }
-  if (is.eof()) {
-    graph = g;
+  if (is.eof() && !edges.empty()) {
+    graph = Graph{edges};
     return is;
   }
   throw std::runtime_error("failed parsing Graph");
 }
 
+constexpr auto sum{std::bind(my_std::ranges::fold_left, std::placeholders::_1, 0, std::plus{})};
+
 auto find_part1(Graph graph) {
   // Stoer–Wagner algorithm
   // https://en.wikipedia.org/wiki/Stoer%E2%80%93Wagner_algorithm
-  auto best_weight{std::numeric_limits<int>::max()};
-  auto best_cut{""s};
-  const auto nodes{graph.nodes()};
+  // (2024-01-06)
+
+  auto nodes{graph.nodes()};
   auto cuts{
-      views::zip(
-          nodes,
-          views::transform(
-              nodes,
-              [](const auto& n) -> std::unordered_set<std::string> { return {n}; }
-          )
-      )
-      | ranges::to<std::unordered_map>()
+      views::transform(nodes, [](const auto& n) -> std::unordered_set<Graph::Node> { return {n}; })
+      | ranges::to<std::vector>()
   };
+  const auto graph_size{nodes.size()};
 
-  for (auto phase{0uz}; phase < nodes.size() - 1; ++phase) {
-    auto s{""s};
-    auto t{""s};
-    {
-      auto q = graph.nodes();
-      s = t = q.back();
+  auto best_weight{std::numeric_limits<Graph::Weight>::max()};
+  auto best_cut{0uz};
+
+  for (auto phase{0uz}; phase < graph_size - 1; ++phase) {
+    auto s{nodes.front()};
+    std::vector<Graph::Node> q;
+    std::unordered_map<Graph::Node, Graph::Weight> cut_weights;
+
+    const auto get_cut_weight{[&](const auto& n) { return cut_weights[n]; }};
+
+    const auto push_max_cut_weight_heap{[&](auto& q, const auto& n, const auto& w) {
+      cut_weights[n] = w;
+      q.push_back(n);
+      ranges::push_heap(q, ranges::less{}, get_cut_weight);
+    }};
+
+    const auto pop_max_cut_weight_heap{[&](auto& q) {
+      ranges::pop_heap(q, ranges::less{}, get_cut_weight);
+      const auto n{q.back()};
       q.pop_back();
+      return n;
+    }};
 
-      std::unordered_set A = {s};
-      const auto get_cut_weight{[&graph, &A](const auto& n) { return graph.cut_weight(n, A); }};
+    for (const auto& n : graph.adjacent.at(s)) {
+      push_max_cut_weight_heap(q, n, graph.edges[s][n]);
+    }
 
-      while (!q.empty()) {
-        s = t;
-        const auto next{ranges::max_element(q, {}, get_cut_weight)};
-        t = *next;
-        q.erase(next);
-        A.insert(t);
+    std::unordered_set A = {s};
+    for (auto i{phase + 1}; i < graph_size - 1; ++i) {
+      s = pop_max_cut_weight_heap(q);
+      A.insert(s);
+      cut_weights.erase(s);
+      for (const auto& n : graph.adjacent.at(s)) {
+        if (!A.contains(n)) {
+          if (cut_weights.contains(n)) {
+            cut_weights[n] += graph.edges[s][n];
+          } else {
+            push_max_cut_weight_heap(q, n, graph.edges[s][n]);
+          }
+        }
       }
     }
-    if (const auto new_weight{graph.node_weight(t)}; new_weight < best_weight) {
+
+    const auto t{pop_max_cut_weight_heap(q)};
+
+    if (const auto new_weight{sum(graph.edges.at(t))}; new_weight < best_weight) {
       best_weight = new_weight;
-      best_cut = t;
+      best_cut = cuts[t].size();
     }
-    for (const auto& [adj_t, adj_t_w] : graph.edges[t]) {
-      if (adj_t != s) {
-        graph.edges[s][adj_t] += adj_t_w;
-        graph.edges[adj_t][s] += adj_t_w;
-      }
-    }
-    for (const auto& n : graph.nodes()) {
-      graph.edges[n].erase(t);
-      if (graph.edges[n].empty()) {
-        graph.edges.erase(n);
-      }
-    }
-    graph.edges.erase(t);
     cuts[s].insert_range(cuts[t]);
-    /* std::print("phase {} s {} t {} bw {} bc {}\n", phase, s, t, best_weight, best_cut); */
+
+    auto w_t{graph.edges[t]};
+    w_t[s] = 0;
+    for (const auto& n : nodes) {
+      graph.edges[s][n] += w_t[n];
+      graph.edges[n][s] += w_t[n];
+    }
+    for (const auto& n : nodes) {
+      graph.erase_edge(n, t);
+      graph.erase_edge(t, n);
+    }
+    nodes.erase(ranges::find(nodes, t));
   }
 
-  const auto bc{cuts[best_cut].size()};
-  return bc * (nodes.size() - bc);
+  return best_cut * (graph_size - best_cut);
 }
 
 int main() {
