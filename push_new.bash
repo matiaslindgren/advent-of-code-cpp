1fed74042ed98c9ed398c0e8da0da2588d4e904a
Matias Lindgren <matias.lindgren@iki.fi>
2024-01-07 00:55:48 +0100

    add git push script

---
+++ push_new.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -xue
+
+if [ $# -ne 2 ]; then
+  printf "usage: $0 year day\n"
+  exit 2
+fi
+
+year="$1"
+day="$2"
+sol=${year}/${day}
+
+pushd txt
+git add correct/${sol}
+git commit -m "solve ${sol}"
+git push
+popd
+
+git add src/${sol}.cpp
+git commit -m "solve ${sol}"
+git push
