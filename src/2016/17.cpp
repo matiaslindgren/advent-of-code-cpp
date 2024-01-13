ad18975e0f51b9a95f01f537925ef391d59ed9ba
Matias Lindgren <matias.lindgren@iki.fi>
2024-01-13 16:23:41 +0100

    solve 2016/17

---
+++ src/2016/17.cpp
@@ -0,0 +1,60 @@
+import std;
+import aoc;
+import md5;
+
+namespace ranges = std::ranges;
+namespace views = std::views;
+
+bool is_open(auto h) {
+  return 0xb <= h && h <= 0xf;
+}
+
+std::pair<std::string, std::string> search(const std::string& passcode) {
+  using std::operator""s;
+
+  std::string part1{}, part2{};
+
+  for (std::deque q = {std::tuple{0, 0, ""s}}; !q.empty(); q.pop_front()) {
+    const auto& [x, y, path] = q.front();
+    if (!(0 <= x && x <= 3 && 0 <= y && y <= 3)) {
+      continue;
+    }
+    if (x == 3 && y == 3) {
+      if (part1.empty() || path.size() < part1.size()) {
+        part1 = path;
+      }
+      if (path.size() > part2.size()) {
+        part2 = path;
+      }
+      continue;
+    }
+    const auto hash{md5::sum32bit(passcode + path) >> 16};
+    if (is_open((hash >> 12) & 0xf)) {
+      q.emplace_back(x, y - 1, path + "U"s);
+    }
+    if (is_open((hash >> 8) & 0xf)) {
+      q.emplace_back(x, y + 1, path + "D"s);
+    }
+    if (is_open((hash >> 4) & 0xf)) {
+      q.emplace_back(x - 1, y, path + "L"s);
+    }
+    if (is_open((hash >> 0) & 0xf)) {
+      q.emplace_back(x + 1, y, path + "R"s);
+    }
+  }
+
+  return {part1, part2};
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+
+  std::string passcode;
+  std::cin >> passcode;
+
+  const auto [part1, part2] = search(passcode);
+
+  std::print("{} {}\n", part1, part2.size());
+
+  return 0;
+}
