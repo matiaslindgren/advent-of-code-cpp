b92ae92af565c48ea7bbea48e82b32493c9bb2f3
Matias Lindgren <matias.lindgren@iki.fi>
2024-01-13 13:52:29 +0100

    rewrite 2016/14

--- src/2016/14.cpp
+++ src/2016/14.cpp
@@ -6,6 +6,31 @@ import md5;
 namespace ranges = std::ranges;
 namespace views = std::views;
 
+constexpr auto chunk_size{512u};
+std::vector<std::thread> threads(aoc::cpu_count());
+std::vector<std::string> checksums(threads.size() * chunk_size);
+
+struct md5sum_repeated {
+  void operator()(std::string_view msg, const auto i_begin, const auto n_repeat, const auto stride)
+      const {
+    for (auto&& [i, out] :
+         my_std::views::enumerate(checksums | views::drop(stride) | views::take(chunk_size))) {
+      auto checksum{std::format("{}{:d}", msg, i_begin + stride + i)};
+      for (int r{}; r < n_repeat + 1; ++r) {
+        checksum = md5::hexdigest(md5::sum(checksum));
+      }
+      out = checksum;
+    }
+  }
+};
+
+void fill_checksums_parallel(std::string_view msg, const auto i, const auto stretch_count) {
+  for (auto&& [t, th] : my_std::views::enumerate(threads)) {
+    th = std::thread(md5sum_repeated{}, msg, i, stretch_count, t * chunk_size);
+  }
+  ranges::for_each(threads, [](auto& th) { th.join(); });
+}
+
 // TODO ranges::adjacent
 // TODO template index sequence
 constexpr decltype(auto) window5(ranges::range auto&& r) {
@@ -13,50 +38,49 @@ constexpr decltype(auto) window5(ranges::range auto&& r) {
 }
 
 auto stretch_search(std::string_view msg, const int stretch_count = 0) {
-  using std::operator""s;
-
-  std::unordered_map<char, std::set<int>> r3s, r5s;
   std::set<int> keys;
 
-  for (int i{}; keys.size() < 70 && i < 1'000'000; ++i) {
-    auto checksum{std::format("{}{:d}", msg, i)};
-    for (int s{}; s < stretch_count + 1; ++s) {
-      checksum = md5::hexdigest(md5::sum(checksum));
+  std::unordered_map<char, std::unordered_set<int>> repeat3;
+  int latest_r3{};
+
+  for (int i{}; i < 5'000'000; ++i) {
+    if (i % checksums.size() == 0) {
+      fill_checksums_parallel(msg, i, stretch_count);
     }
-    bool r3{false};
-    for (const auto [ch0, ch1, ch2, ch3, ch4] : window5(checksum)) {
-      if (!r3) {
-        if (ch0 == ch1 && ch1 == ch2) {
-          r3s[ch0].insert(i);
-          r3 = true;
-        } else if (ch1 == ch2 && ch2 == ch3) {
-          r3s[ch1].insert(i);
-          r3 = true;
-        } else if (ch2 == ch3 && ch3 == ch4) {
-          r3s[ch2].insert(i);
-          r3 = true;
+    bool found_r3{false};
+    for (const auto& [a, b, c, d, e] : window5(checksums.at(i % checksums.size()))) {
+      if (!found_r3) {
+        if (a == b && b == c) {
+          repeat3[a].insert(i);
+          found_r3 = true;
+        } else if (b == c && c == d) {
+          repeat3[b].insert(i);
+          found_r3 = true;
+        } else if (c == d && d == e) {
+          repeat3[c].insert(i);
+          found_r3 = true;
         }
       }
-      if (ch0 == ch1 && ch1 == ch2 && ch2 == ch3 && ch3 == ch4) {
-        r5s[ch0].insert(i);
+      if (found_r3 && keys.size() < 64) {
+        latest_r3 = i;
       }
-    }
-    for (const auto& ch : r5s | views::keys) {
-      for (const auto& r5_i : r5s[ch]) {
-        for (const auto& r3_i : r3s[ch]) {
-          if (const auto dist{r5_i - r3_i}; 0 < dist && dist <= 1000) {
-            keys.insert(r3_i);
+      if (a == b && b == c && c == d && d == e) {
+        for (const auto& i_prev : repeat3[a]) {
+          if (const auto dist{i - i_prev}; 0 < dist && dist <= 1000) {
+            keys.insert(i_prev);
           }
         }
       }
+    }
+    if (keys.size() > 63 && i - latest_r3 > 1000) {
+      break;
     }
   }
 
   if (const auto key{ranges::begin(views::drop(keys, 63))}; key != keys.end()) {
     return *key;
   }
-
-  return std::numeric_limits<int>::max();
+  throw std::runtime_error("search space exhausted with no key found");
 }
 
 int main() {
