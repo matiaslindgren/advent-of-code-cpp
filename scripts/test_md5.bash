4327613eab0c7416b71ab18bb91b6275b4b0e3bd
Matias Lindgren <matias.lindgren@iki.fi>
2024-01-13 16:08:25 +0100

    fix md5 test

--- scripts/test_md5.bash
+++ scripts/test_md5.bash
@@ -7,11 +7,31 @@ function rm_tmpdir {
 }
 trap rm_tmpdir EXIT
 
-for n in $(seq 1000 1000 20000); do
+function test_with_random_data {
+  local bs="$1"
+  local n="$2"
   data="${tmpdir}/rand.data"
-  dd if=/dev/urandom of="$data" bs=1024 count=${n} 2> /dev/null
+  dd if=/dev/urandom of="$data" bs=${bs} count=${n} 2> /dev/null
   wc -c "$data"
   aoc_md5=$(make FAST=${FAST} run_tools/md5 < "$data")
   gnu_md5=$(md5sum < "$data" | cut -f1 -d' ')
-  [ "$aoc_md5" == "$gnu_md5" ] || exit 1
+  if [ "$aoc_md5" != "$gnu_md5" ]; then
+    printf "aoc_md5 '$aoc_md5' != '$gnu_md5' gnu_md5\n"
+    cp "$data" failing_md5_test_data
+    return 1
+  fi
+  rm "$data"
+  return 0
+}
+
+for n in $(seq 1 1 256); do
+  if ! test_with_random_data 1 $n; then
+    exit 1;
+  fi
+done
+
+for n in $(seq 1000 1000 20000); do
+  if ! test_with_random_data 1024 $n; then
+    exit 1;
+  fi
 done
