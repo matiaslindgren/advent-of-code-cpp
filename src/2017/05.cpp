4b8c707a92dee01d56bff33667df150806a98c15
Matias Lindgren <matias.lindgren@iki.fi>
2024-01-15 21:29:30 +0100

    solve 2017/05

---
+++ src/2017/05.cpp
@@ -0,0 +1,30 @@
+import std;
+import aoc;
+
+namespace ranges = std::ranges;
+namespace views = std::views;
+
+auto find_exit(auto jumps, int limit = std::numeric_limits<int>::max()) {
+  int jump_count{};
+  for (auto j{0uz}; 0 <= j && j < jumps.size(); ++jump_count) {
+    const auto n{jumps[j]};
+    jumps[j] += 1 - 2 * (n >= limit);
+    j += n;
+  }
+  return jump_count;
+}
+
+int main() {
+  std::istringstream input{aoc::slurp_file("/dev/stdin")};
+  const auto jumps{views::istream<int>(input) | ranges::to<std::vector<int>>()};
+  if (jumps.empty()) {
+    throw std::runtime_error("cannot do anything with empty input");
+  }
+
+  const auto part1{find_exit(jumps)};
+  const auto part2{find_exit(jumps, 3)};
+
+  std::print("{} {}\n", part1, part2);
+
+  return 0;
+}
