cb39b7252440dbd5388a860be08cae420cde57d7
Matias Lindgren <matias.lindgren@iki.fi>
2024-01-15 22:16:01 +0100

    solve 2017/06

---
+++ src/2017/06.cpp
@@ -0,0 +1,53 @@
+import std;
+import aoc;
+import my_std;
+
+namespace ranges = std::ranges;
+namespace views = std::views;
+
+struct Bank {
+  std::vector<int> v;
+  bool operator==(const Bank& bank) const {
+    return ranges::equal(v, bank.v);
+  }
+};
+
+template <>
+struct std::hash<Bank> {
+  std::size_t operator()(const Bank& bank) const noexcept {
+    return my_std::ranges::fold_left(bank.v, 0uz, [](auto h, auto x) { return (h << 4) | x; });
+  }
+};
+
+auto find_loop(Bank bank) {
+  int cycle{};
+  std::unordered_map<Bank, int> seen;
+  for (; !seen.contains(bank); ++cycle) {
+    seen[bank] = cycle;
+
+    const auto begin{ranges::max_element(bank.v)};
+    auto blocks{std::exchange(*begin, 0u)};
+
+    const auto n{bank.v.size()};
+    for (auto i{((begin - bank.v.begin()) + 1) % n}; blocks; i = (i + 1) % n) {
+      bank.v[i] += 1;
+      blocks -= 1;
+    }
+  }
+  return std::pair{cycle, cycle - seen[bank]};
+}
+
+int main() {
+  std::istringstream input{aoc::slurp_file("/dev/stdin")};
+  const auto bank{views::istream<int>(input) | ranges::to<std::vector<int>>()};
+  if (bank.size() > 16 || !ranges::all_of(bank, [](auto x) { return 0 <= x && x <= 15; })) {
+    throw std::runtime_error(
+        "invalid input, input size must be at most 16 and every element must fit into 4 bits"
+    );
+  }
+
+  const auto [part1, part2] = find_loop(Bank{bank});
+  std::print("{} {}\n", part1, part2);
+
+  return 0;
+}
