f9df70238bcd27eb149453c8f55bad3ff5b43b26
Matias Lindgren <matias.lindgren@iki.fi>
2024-01-15 19:46:28 +0100

    solve 2017/01

---
+++ src/2017/01.cpp
@@ -0,0 +1,37 @@
+import std;
+import aoc;
+import my_std;
+
+namespace ranges = std::ranges;
+namespace views = std::views;
+
+auto parse_digits(std::istream& is) {
+  return views::istream<char>(is)
+         | views::transform([](char ch) { return std::max('0', std::min('9', ch)) - '0'; })
+         | ranges::to<std::vector<int>>();
+}
+
+constexpr auto sum{std::bind(my_std::ranges::fold_left, std::placeholders::_1, 0, std::plus{})};
+
+int solve(const auto& digits, const auto n) {
+  return sum(my_std::views::enumerate(digits) | views::transform([&](const auto& p) {
+               const auto [i, a] = p;
+               const auto b{digits[(i + n) % digits.size()]};
+               return (a == b) * a;
+             }));
+}
+
+int main() {
+  std::istringstream input{aoc::slurp_file("/dev/stdin")};
+  const auto digits{parse_digits(input)};
+  if (digits.empty() || digits.size() % 2 != 0) {
+    throw std::runtime_error("input needs to contain an even amount of digits");
+  }
+
+  const auto part1{solve(digits, 1)};
+  const auto part2{solve(digits, digits.size() / 2)};
+
+  std::print("{} {}\n", part1, part2);
+
+  return 0;
+}
