e21204b9a6bb405ad0e6af927bfe67968acb51a2
Matias Lindgren <matias.lindgren@iki.fi>
2024-01-19 20:01:26 +0100

    solve 2017/11

---
+++ src/2017/11.cpp
@@ -0,0 +1,69 @@
+import std;
+import aoc;
+
+namespace ranges = std::ranges;
+namespace views = std::views;
+
+struct Hex {
+  int q{}, r{};
+
+  Hex operator+(const Hex& rhs) const {
+    return {q + rhs.q, r + rhs.r};
+  }
+
+  auto distance(const Hex& rhs) const {
+    const auto dq{std::abs(q - rhs.q)};
+    const auto ds{std::abs((q + r) - (rhs.q + rhs.r))};
+    const auto dr{std::abs(r - rhs.r)};
+    return (dq + ds + dr) / 2;
+  }
+};
+
+std::istream& operator>>(std::istream& is, Hex& h) {
+  if (char c1; is >> c1 && (c1 == 'n' || c1 == 's')) {
+    if (char c2; (is.peek() == 'w' || is.peek() == 'e') && is >> c2) {
+      if (c1 == 'n' && c2 == 'w') {
+        h = {.q = -1, .r = 0};
+      } else if (c1 == 'n' && c2 == 'e') {
+        h = {.q = 1, .r = -1};
+      } else if (c1 == 's' && c2 == 'w') {
+        h = {.q = -1, .r = 1};
+      } else if (c1 == 's' && c2 == 'e') {
+        h = {.q = 1, .r = 0};
+      } else {
+        is.setstate(std::ios_base::failbit);
+      }
+    } else {
+      h = {.q = 0, .r = c1 == 'n' ? -1 : 1};
+    }
+  }
+  if (is || is.eof()) {
+    return is;
+  }
+  throw std::runtime_error("failed parsing Hex");
+}
+
+auto parse_steps(std::string path) {
+  auto input{aoc::slurp_file(path)};
+  ranges::replace(input, ',', ' ');
+  std::istringstream is{input};
+  return views::istream<Hex>(is) | ranges::to<std::vector>();
+}
+
+auto measure_distances(const auto& steps) {
+  const Hex begin{0, 0};
+  auto pos{begin};
+  int max_dist{};
+  for (const Hex& step : steps) {
+    pos = pos + step;
+    max_dist = std::max(max_dist, begin.distance(pos));
+  }
+  return std::pair{begin.distance(pos), max_dist};
+}
+
+int main() {
+  const auto steps{parse_steps("/dev/stdin")};
+  const auto [part1, part2] = measure_distances(steps);
+  std::print("{} {}\n", part1, part2);
+  return 0;
+}
