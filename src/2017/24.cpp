7b7d6010d9e6a8e5dcd0dd955d91c2733e806a6a
Matias Lindgren <matias.lindgren@iki.fi>
2024-01-25 23:22:03 +0100

    solve 2017/24

---
+++ src/2017/24.cpp
@@ -0,0 +1,72 @@
+import std;
+import aoc;
+import my_std;
+
+namespace ranges = std::ranges;
+namespace views = std::views;
+
+struct Component {
+  int a{}, b{};
+  int strength() const {
+    return a + b;
+  }
+};
+
+std::istream& operator>>(std::istream& is, Component& comp) {
+  using aoc::skip;
+  using std::operator""s;
+  if (int a, b; is >> a && skip(is, "/"s) && is >> b) {
+    comp = {a, b};
+  }
+  if (is || is.eof()) {
+    return is;
+  }
+  throw std::runtime_error("failed parsing Component");
+}
+
+auto find_max_bridges(auto components) {
+  constexpr auto state_size{54};
+  if (components.size() > state_size) {
+    throw std::runtime_error("too many components, cannot use std::bitset");
+  }
+
+  int max_part1{}, max_part2{}, max_len{};
+
+  for (std::deque q = {std::tuple{0, 0, std::bitset<state_size>{}}}; !q.empty(); q.pop_front()) {
+    const auto [port, strength, used] = q.front();
+
+    max_part1 = std::max(max_part1, strength);
+
+    if (const auto n{used.count()}; n > max_len) {
+      max_len = n;
+      max_part2 = strength;
+    } else if (n == max_len) {
+      max_part2 = std::max(max_part2, strength);
+    }
+
+    for (const auto [i, comp] : my_std::views::enumerate(components)) {
+      if (!used[i] && (port == comp.a || port == comp.b)) {
+        auto used_next{used};
+        used_next[i] = true;
+        if (port == comp.a) {
+          q.emplace_back(comp.b, strength + comp.strength(), used_next);
+        }
+        if (port == comp.b) {
+          q.emplace_back(comp.a, strength + comp.strength(), used_next);
+        }
+      }
+    }
+  }
+
+  return std::pair{max_part1, max_part2};
+}
+
+int main() {
+  std::istringstream input{aoc::slurp_file("/dev/stdin")};
+  const auto components{views::istream<Component>(input) | ranges::to<std::vector>()};
+
+  const auto [part1, part2] = find_max_bridges(components);
+  std::print("{} {}\n", part1, part2);
+
+  return 0;
+}
