68edefb270f0ed6ea345d6c3ea732072ab69b55b
Matias Lindgren <matias.lindgren@iki.fi>
2024-01-30 22:19:22 +0100

    solve 2018/11

---
+++ src/2018/11.cpp
@@ -0,0 +1,70 @@
+import std;
+
+namespace ranges = std::ranges;
+namespace views = std::views;
+
+int get_digit(auto a, const auto k) {
+  int d{};
+  for (int i{}; i < k; ++i) {
+    d = a % 10;
+    a /= 10;
+  }
+  return d;
+}
+
+struct Result {
+  long power{};
+  std::size_t x{}, y{}, size{};
+};
+
+auto search(const int serial) {
+  // https://en.wikipedia.org/wiki/Summed-area_table
+  constexpr auto size{301uz};
+  std::vector<long> sums(size * size, 0);
+  std::mdspan grid(sums.data(), size, size);
+  for (auto y{1uz}; y < size; ++y) {
+    for (auto x{1uz}; x < size; ++x) {
+      const auto power{get_digit((x + 11) * ((x + 11) * (y + 1) + serial), 3) - 5};
+      grid[x, y] = power + grid[x, y - 1] + grid[x - 1, y] - grid[x - 1, y - 1];
+    }
+  }
+  std::vector<Result> results;
+  for (auto n{1uz}; n < size; ++n) {
+    auto& r{results.emplace_back(std::numeric_limits<long>::min(), 0, 0, n)};
+    for (auto y{1uz}; y < size - n; ++y) {
+      for (auto x{1uz}; x < size - n; ++x) {
+        const auto a{grid[x + 0, y + 0]};
+        const auto b{grid[x + n, y + 0]};
+        const auto c{grid[x + 0, y + n]};
+        const auto d{grid[x + n, y + n]};
+        if (const auto power{d - c - b + a}; power > r.power) {
+          r.power = power;
+          r.x = x + 2;
+          r.y = y + 2;
+        }
+      }
+    }
+  }
+  return results;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+
+  int serial;
+  std::cin >> serial;
+
+  const auto results{search(serial)};
+
+  const auto sq_3{results[2]};
+  const auto part1{std::format("{},{}", sq_3.x, sq_3.y)};
+
+  const auto sq_max{ranges::max_element(results, ranges::less{}, [](const auto& r) {
+    return r.power;
+  })};
+  const auto part2{std::format("{},{},{}", sq_max->x, sq_max->y, sq_max->size)};
+
+  std::print("{} {}\n", part1, part2);
+
+  return 0;
+}
