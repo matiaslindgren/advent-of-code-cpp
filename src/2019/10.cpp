7380bd33771ba7f2f8421957961e63511ec846e5
Matias Lindgren <matias.lindgren@iki.fi>
2024-03-12 00:10:12 +0100

    simplify

--- src/2019/10.cpp
+++ src/2019/10.cpp
@@ -8,39 +8,32 @@ namespace ranges = std::ranges;
 namespace views = std::views;
 
 using aoc::Vec2;
+using Points = std::vector<Vec2>;
 
-struct Grid {
-  std::vector<bool> is_asteroid;
-  std::size_t width{};
-
-  auto asteroids() const {
-    return views::iota(0uz, is_asteroid.size())
-           | views::filter([this](auto i) { return is_asteroid.at(i); })
-           | views::transform([this](int i) {
-               auto [y, x]{std::div(i, width)};
-               return Vec2{y, x};
-             });
-  }
-};
-
-Grid parse_grid(std::istream& is) {
-  Grid g;
-  for (std::string line; std::getline(is, line) and not line.empty();) {
-    if (not g.width) {
-      g.width = line.size();
-    } else if (line.size() != g.width) {
-      throw std::runtime_error("every line must be of same width");
-    }
-    for (char ch : line) {
-      if (ch == '#' or ch == '.') {
-        g.is_asteroid.push_back(ch == '#');
-      } else {
-        throw std::runtime_error("every tile must be . or #");
+Points parse_asteroids(std::istream& is) {
+  Points asteroids;
+  {
+    auto width{0uz};
+    Vec2 pos;
+    for (std::string line; std::getline(is, line) and not line.empty(); pos.y += 1) {
+      if (not width) {
+        width = line.size();
+      } else if (line.size() != width) {
+        throw std::runtime_error("every line must be of same width");
+      }
+      pos.x = 0;
+      for (char ch : line) {
+        if (ch == '#') {
+          asteroids.push_back(pos);
+        } else if (ch != '.') {
+          throw std::runtime_error("every tile must be . or #");
+        }
+        pos.x += 1;
       }
     }
   }
   if (is.eof()) {
-    return g;
+    return asteroids;
   }
   throw std::runtime_error("failed parsing grid");
 }
@@ -52,19 +45,18 @@ double angle(const Vec2& p1, const Vec2& p2) {
   return a < 0 ? a + 2.0 * pi : a;
 }
 
-auto search(const Grid& grid) {
+auto search(Points asteroids) {
   Vec2 laser;
   auto max_seen{0uz};
 
-  for (auto p1 : grid.asteroids()) {
+  for (const auto& p1 : asteroids) {
     std::unordered_set<Vec2> seen;
-    for (auto p2 : grid.asteroids()) {
-      if (p1 == p2) {
-        continue;
-      }
-      const Vec2 d{p2 - p1};
-      if (const auto gcd{std::gcd(d.y, d.x)}) {
-        seen.insert(p1 + d / gcd);
+    for (const auto& p2 : asteroids) {
+      if (p1 != p2) {
+        const Vec2 d{p2 - p1};
+        if (const auto gcd{std::gcd(d.y, d.x)}) {
+          seen.insert(p1 + d / gcd);
+        }
       }
     }
     if (seen.size() > max_seen) {
@@ -73,23 +65,24 @@ auto search(const Grid& grid) {
     }
   }
 
-  auto targets{grid.asteroids() | ranges::to<std::vector>()};
-  ranges::sort(targets, {}, [laser](const Vec2& target) {
+  ranges::sort(asteroids, {}, [laser](const Vec2& target) {
     return std::tuple{angle(laser, target), laser.distance(target)};
   });
 
   std::unordered_set<Vec2> destroyed;
   double prev_angle{-1};
 
-  for (const Vec2& target : targets) {
+  for (const Vec2& target : asteroids) {
     if (destroyed.contains(target)) {
       continue;
     }
+
     if (auto a{angle(laser, target)}; a == prev_angle) {
       continue;
     } else {
       prev_angle = a;
     }
+
     destroyed.insert(target);
     if (destroyed.size() == 200) {
       return std::pair{max_seen, target.x * 100 + target.y};
@@ -101,10 +94,7 @@ auto search(const Grid& grid) {
 
 int main() {
   std::istringstream input{aoc::slurp_file("/dev/stdin")};
-  const Grid grid{parse_grid(input)};
-
-  const auto [part1, part2]{search(grid)};
+  const auto [part1, part2]{search(parse_asteroids(input))};
   std::print("{} {}\n", part1, part2);
-
   return 0;
 }
