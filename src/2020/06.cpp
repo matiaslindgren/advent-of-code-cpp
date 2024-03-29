e9c531d92466fbd4f9c264a751fc83943e2fe5f3
Matias Lindgren <matias.lindgren@iki.fi>
2024-03-29 13:49:39 +0100

    solve 2020/06

--- src/2020/06.cpp
+++ src/2020/06.cpp
@@ -1,10 +1,41 @@
 import std;
 import aoc;
 
+using std::operator""s;
+using std::operator""sv;
+
+namespace ranges = std::ranges;
+namespace views = std::views;
+
+using CharBits = std::bitset<'z' - 'a' + 1>;
+
+auto search(std::string_view path) {
+  int part1{}, part2{};
+  for (auto&& section : views::split(aoc::slurp_file(path), "\n\n"s)) {
+    CharBits any, all;
+    all.set();
+    for (auto&& line : views::split(section, "\n"sv)) {
+      if (not line.empty()) {
+        CharBits ans;
+        for (char ch : line) {
+          if ('a' <= ch and ch <= 'z') {
+            ans.set('z' - ch);
+          } else {
+            throw std::runtime_error("all input chars must be in [a-z]");
+          }
+        }
+        any |= ans;
+        all &= ans;
+      }
+    }
+    part1 += any.count();
+    part2 += all.count();
+  }
+  return std::pair{part1, part2};
+}
+
 int main() {
-  // TODO implement
-  const auto input{aoc::slurp_file("/dev/stdin")};
-  (void)input;
-  std::cout << aoc::slurp_file("txt/correct/2020/06");
+  const auto [part1, part2]{search("/dev/stdin")};
+  std::print("{} {}\n", part1, part2);
   return 0;
 }
