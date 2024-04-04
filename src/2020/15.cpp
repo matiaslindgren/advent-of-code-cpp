6d6fda41a6e780ea22344fb4f985beaa724d1c5b
Matias Lindgren <matias.lindgren@iki.fi>
2024-04-04 19:45:22 +0200

    solve 2020/15

--- src/2020/15.cpp
+++ src/2020/15.cpp
@@ -1,10 +1,42 @@
 import std;
+import my_std;
 import aoc;
 
+auto search(const auto& items, const int n) {
+  std::unordered_map<int, int> age1, age2;
+  for (auto [t, x] : my_std::views::enumerate(items, 1)) {
+    age1[x] = age2[x] = t;
+  }
+  int t0{items.back()};
+  for (auto t1{items.size() + 1}; t1 <= n; ++t1) {
+    t0 = age2[t0] - age1[t0];
+    if ((age1[t0] = std::exchange(age2[t0], t1)) == 0) {
+      age1[t0] = t1;
+    }
+  }
+  return t0;
+}
+
+auto parse_input(std::string_view path) {
+  std::ios::sync_with_stdio(false);
+  using aoc::skip;
+  using std::operator""s;
+
+  std::vector<int> items;
+  for (int x; std::cin >> x;) {
+    items.push_back(x);
+    std::cin >> skip(","s);
+  }
+  return items;
+}
+
 int main() {
-  // TODO implement
-  const auto input{aoc::slurp_file("/dev/stdin")};
-  (void)input;
-  std::cout << aoc::slurp_file("txt/correct/2020/15");
+  const auto items{parse_input("/dev/stdin")};
+
+  const auto part1{search(items, 2020)};
+  const auto part2{search(items, 30'000'000)};
+
+  std::print("{} {}\n", part1, part2);
+
   return 0;
 }
