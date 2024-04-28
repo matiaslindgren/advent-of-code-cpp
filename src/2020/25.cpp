14cd50da1a9713bec819741e567f592a1b37d784
Matias Lindgren <matias.lindgren@iki.fi>
2024-04-28 18:30:12 +0200

    solve 2020/25

--- src/2020/25.cpp
+++ src/2020/25.cpp
@@ -1,10 +1,36 @@
-#include "aoc.hpp"
 #include "std.hpp"
 
+long step(const long x, const long subject) {
+  return (x * subject) % 20201227;
+}
+
+long transform(const long subject, const long loop_size) {
+  long x{1};
+  for (long l{1}; l <= loop_size; ++l) {
+    x = step(x, subject);
+  }
+  return x;
+}
+
+long search_loop_size(const long subject, const long key) {
+  long x{1};
+  for (long l{};; ++l) {
+    if (x == key) {
+      return l;
+    }
+    x = step(x, subject);
+  }
+}
+
+auto search(const int card, const int door) {
+  return transform(card, search_loop_size(7, door));
+}
+
 int main() {
-  // TODO implement
-  const auto input{aoc::slurp_file("/dev/stdin")};
-  (void)input;
-  std::cout << aoc::slurp_file("txt/correct/2020/25");
+  if (int card, door; std::cin >> card >> door) {
+    std::print("{}\n", search(card, door));
+  } else {
+    throw std::runtime_error("failed parsing a pair of integers");
+  }
   return 0;
 }
