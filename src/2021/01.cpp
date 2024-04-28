fee454a380a3f4a80c94dad95e3a69b368b577df
Matias Lindgren <matias.lindgren@iki.fi>
2024-04-28 23:07:48 +0200

    solve 2021/01

--- src/2021/01.cpp
+++ src/2021/01.cpp
@@ -1,5 +1,38 @@
-// TODO
+#include "aoc.hpp"
+#include "my_std.hpp"
 #include "std.hpp"
+
+namespace ranges = std::ranges;
+namespace views = std::views;
+
+// TODO (llvm19) ranges::adjacent
+auto window2(ranges::range auto&& r) {
+  return views::zip(r, views::drop(r, 1));
+}
+auto window3(ranges::range auto&& r) {
+  return views::zip(r, views::drop(r, 1), views::drop(r, 2));
+}
+
+constexpr auto sum{std::__bind_back(ranges::fold_left, 0, std::plus{})};
+
+auto find_part1(const auto& depths) {
+  return sum(window2(depths) | views::transform(my_std::apply_fn(std::less{})));
+}
+
+auto find_part2(const auto& depths) {
+  return find_part1(
+      window3(depths) | views::transform([](auto&& w) {
+        auto [x1, x2, x3]{w};
+        return x1 + x2 + x3;
+      })
+      | ranges::to<std::vector>()
+  );
+}
+
 int main() {
-  std::system("cat txt/correct/2021/01");
+  const auto depths{aoc::slurp<int>("/dev/stdin")};
+  const auto part1{find_part1(depths)};
+  const auto part2{find_part2(depths)};
+  std::print("{} {}\n", part1, part2);
+  return 0;
 }
