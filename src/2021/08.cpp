63b9d8d89aa7a82d511c048ddea80d5a58826e1c
Matias Lindgren <matias.lindgren@iki.fi>
2024-05-02 12:27:04 +0200

    solve 2021/08

--- src/2021/08.cpp
+++ src/2021/08.cpp
@@ -1,5 +1,74 @@
-// TODO
+#include "aoc.hpp"
 #include "std.hpp"
+
+namespace ranges = std::ranges;
+namespace views = std::views;
+
+using std::operator""s;
+using Strings = std::vector<std::string>;
+
+constexpr auto digit_map{"4725360918"s};
+constexpr auto sum{std::__bind_back(ranges::fold_left, 0, std::plus{})};
+
+struct Entry {
+  Strings inputs;
+  Strings output;
+
+  auto decode() const {
+    // idea from
+    // https://www.reddit.com/r/adventofcode/comments/rbj87a/comment/hnp4saz
+    // accessed 2024-05-01
+    std::unordered_map<char, int> input_counts;
+    for (char ch : inputs | views::join) {
+      input_counts[ch] += 1;
+    }
+    return output | views::transform([&](const std::string& s) {
+             return sum(views::transform(s, [&](char ch) { return input_counts[ch]; }));
+           })
+           | views::transform([&](int count) {
+               auto idx{((count / 2) % 15) % 11};
+               return digit_map.at(idx);
+             })
+           | ranges::to<std::string>();
+  }
+};
+
+std::istream& operator>>(std::istream& is, Entry& entry) {
+  if (std::string line; std::getline(is, line) and not line.empty()) {
+    std::istringstream ls{line};
+    Strings inputs;
+    for (std::string item; ls >> item and item != "|"s;) {
+      inputs.push_back(item);
+    }
+    Strings output;
+    for (std::string item; ls >> item;) {
+      output.push_back(item);
+    }
+    if (ls.eof() and not inputs.empty() and not output.empty()) {
+      entry = {inputs, output};
+    } else {
+      throw std::runtime_error(std::format("failed parsing line {}", line));
+    }
+  }
+  return is;
+}
+
+auto search(const auto& entries) {
+  int part1{};
+  int part2{};
+  for (const Entry& e : entries) {
+    const auto output{e.decode()};
+    for (char ch : output) {
+      part1 += "1478"s.contains(ch);
+    }
+    part2 += std::stoi(output);
+  }
+  return std::pair{part1, part2};
+}
+
 int main() {
-  std::system("cat txt/correct/2021/08");
+  const auto entries{aoc::slurp<Entry>("/dev/stdin")};
+  const auto [part1, part2]{search(entries)};
+  std::print("{} {}\n", part1, part2);
+  return 0;
 }
