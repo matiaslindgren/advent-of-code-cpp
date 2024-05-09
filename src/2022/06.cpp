6c8d2dae7bb10fdf6b9a3b7b677a599eb21dfe4e
Matias Lindgren <matias.lindgren@iki.fi>
2024-05-09 22:48:07 +0200

    solve 2022/06

--- src/2022/06.cpp
+++ src/2022/06.cpp
@@ -1,5 +1,27 @@
-// TODO
+#include "aoc.hpp"
 #include "std.hpp"
+
+namespace ranges = std::ranges;
+namespace views = std::views;
+
+auto find_distinct(std::string input, std::size_t msg_len) {
+  auto rhs{msg_len};
+  for (; rhs < input.size(); ++rhs) {
+    auto window{input.substr(rhs - msg_len, msg_len)};
+    if (ranges::to<std::unordered_set>(window).size() == msg_len) {
+      break;
+    }
+  }
+  return rhs;
+}
+
 int main() {
-  std::system("cat txt/correct/2022/06");
+  const auto input{aoc::slurp_file("/dev/stdin")};
+
+  const auto part1{find_distinct(input, 4)};
+  const auto part2{find_distinct(input, 14)};
+
+  std::print("{} {}\n", part1, part2);
+
+  return 0;
 }
