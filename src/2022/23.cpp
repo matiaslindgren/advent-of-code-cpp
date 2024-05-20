11651a7f62ae9e7bf07413b7f8fb9657b698137b
Matias Lindgren <matias.lindgren@iki.fi>
2024-05-20 17:30:44 +0200

    solve 2022/23

--- src/2022/23.cpp
+++ src/2022/23.cpp
@@ -1,5 +1,143 @@
-// TODO
+#include "aoc.hpp"
+#include "ndvec.hpp"
 #include "std.hpp"
+
+namespace ranges = std::ranges;
+namespace views = std::views;
+
+using Vec2 = ndvec::vec2<int>;
+
+enum struct Direction {
+  north,
+  south,
+  west,
+  east,
+};
+
+struct Grid {
+  std::unordered_set<Vec2> elves;
+
+  bool has_adjacent_elf(Vec2 pos, Vec2 dir) const {
+    for (Vec2 d(-1, -1); d.y() <= 1; d.y() += 1) {
+      for (d.x() = -1; d.x() <= 1; d.x() += 1) {
+        if ((dir.x() == 0 or d.x() == dir.x()) and (dir.y() == 0 or d.y() == dir.y())) {
+          if (Vec2 adj{pos + d}; adj != pos and elves.contains(adj)) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  bool has_adjacent_elves(Vec2 pos) const {
+    return ranges::any_of(
+        std::array{Vec2(0, 1), Vec2(0, -1), Vec2(1, 0), Vec2(-1, 0)},
+        [pos, this](Vec2 dir) { return has_adjacent_elf(pos, dir); }
+    );
+  }
+
+  std::optional<Vec2> find_move(Vec2 pos, Direction start_dir) const {
+    for (int turn{}; turn < 4; ++turn) {
+      Vec2 dir;
+      switch (Direction{(std::to_underlying(start_dir) + turn) % 4}) {
+        case Direction::north: {
+          dir = Vec2(0, -1);
+        } break;
+        case Direction::south: {
+          dir = Vec2(0, 1);
+        } break;
+        case Direction::west: {
+          dir = Vec2(-1, 0);
+        } break;
+        case Direction::east: {
+          dir = Vec2(1, 0);
+        } break;
+      }
+      if (not has_adjacent_elf(pos, dir)) {
+        return pos + dir;
+      }
+    }
+    return std::nullopt;
+  }
+};
+
+auto find_part1(const Grid& g) {
+  const auto [p_min, p_max]{ranges::fold_left(
+      g.elves,
+      std::pair{Vec2(), Vec2()},
+      [](auto minmax, const Vec2& p) {
+        return std::pair{minmax.first.min(p), minmax.second.max(p)};
+      }
+  )};
+  int n{};
+  for (Vec2 p{p_min}; p.y() <= p_max.y(); p.y() += 1) {
+    for (p.x() = p_min.x(); p.x() <= p_max.x(); p.x() += 1) {
+      n += not g.elves.contains(p);
+    }
+  }
+  return n;
+}
+
+auto search(Grid grid) {
+  std::optional<int> part1{}, part2{};
+
+  auto dir{Direction::north};
+  for (int round{}; round < 10'000 and (not part1 or not part2); ++round) {
+    std::unordered_map<Vec2, Vec2> moves;
+    std::unordered_map<Vec2, int> target_counts;
+
+    for (Vec2 pos : grid.elves) {
+      if (grid.has_adjacent_elves(pos)) {
+        if (auto dst{grid.find_move(pos, dir)}) {
+          moves[pos] = *dst;
+          target_counts[*dst] += 1;
+        }
+      }
+    }
+
+    for (auto [src, dst] : moves) {
+      if (target_counts[dst] == 1) {
+        grid.elves.erase(src);
+        grid.elves.insert(dst);
+      }
+    }
+
+    if (round == 10) {
+      part1 = find_part1(grid);
+    }
+    if (ranges::count(views::values(target_counts), 1) == 0) {
+      part2 = round + 1;
+    }
+
+    dir = Direction{(std::to_underlying(dir) + 1) % 4};
+  }
+
+  return std::pair{part1.value(), part2.value()};
+}
+
+Grid parse_grid(std::string_view path) {
+  Grid grid;
+  {
+    Vec2 p;
+    for (auto line : aoc::slurp_lines(path)) {
+      p.x() = 0;
+      for (char ch : line) {
+        if (ch == '#') {
+          grid.elves.insert(p);
+        } else if (ch != '.') {
+          throw std::runtime_error(std::format("unknown grid tile '{}'", ch));
+        }
+        p.x() += 1;
+      }
+      p.y() += 1;
+    }
+  }
+  return grid;
+}
+
 int main() {
-  std::system("cat txt/correct/2022/23");
+  const auto [part1, part2]{search(parse_grid("/dev/stdin"))};
+  std::println("{} {}", part1, part2);
+  return 0;
 }
