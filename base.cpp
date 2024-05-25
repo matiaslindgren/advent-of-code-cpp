0e31ea217feaa1527279ca470c175e559796c1da
Matias Lindgren <matias.lindgren@iki.fi>
2024-05-25 19:25:16 +0200

    add default grid parsing logic

--- base.cpp
+++ base.cpp
@@ -1,4 +1,5 @@
 #include "aoc.hpp"
+#include "ndvec.hpp"
 #include "std.hpp"
 
 namespace ranges = std::ranges;
@@ -6,6 +7,7 @@ namespace views = std::views;
 
 using aoc::skip;
 using std::operator""s;
+using Vec2 = ndvec::vec2<int>;
 
 struct Item {
   int id;
@@ -50,6 +52,43 @@ std::istream& operator>>(std::istream& is, Item& item) {
 
 std::ostream& operator<<(std::ostream& os, const Item& item) {
   return os << std::format("{}", item);
+}
+
+enum class Tile : char {
+  rock = '#',
+  none = '.',
+};
+
+struct Grid {
+  std::unordered_map<Vec2, Tile> tiles;
+  std::size_t width;
+};
+
+auto parse_grid(std::string_view path) {
+  Grid g{};
+  Vec2 p;
+  std::istringstream is{aoc::slurp_file(path)};
+  for (std::string line; std::getline(is, line) and not line.empty(); p.y() += 1) {
+    if (g.width == 0U) {
+      g.width = line.size();
+    } else if (g.width != line.size()) {
+      throw std::runtime_error("every row must be of equal length");
+    }
+    p.x() = 0;
+    for (char ch : line) {
+      switch (ch) {
+        case std::to_underlying(Tile::rock):
+        case std::to_underlying(Tile::none): {
+          g.tiles[p] = {ch};
+        } break;
+        default: {
+          throw std::runtime_error(std::format("unknown tile '{}'", ch));
+        }
+      }
+      p.x() += 1;
+    }
+  }
+  return g;
 }
 
 int main() {
